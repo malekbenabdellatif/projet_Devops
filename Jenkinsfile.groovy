ffdaf5fb83ad88987fb47419d11d532cc8002af0
WassimSlim21 <Wassimslim18@gmail.com>
2022-11-08 13:26:10 +0100

    pushing jenkins groovy file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,104 @@
+pipeline {
+    agent any
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "MAVEN"
+    }
+        environment {
+        NEXUS_VERSION = "nexus3"
+        NEXUS_PROTOCOL = "http"
+        NEXUS_URL = "192.168.33.10:8081"
+        NEXUS_REPOSITORY = "deployRepo"
+        NEXUS_CREDENTIAL_ID = "nexus-user-credentials"
+    }
+    
+   stages{
+    stage('GIT') { 
+        steps{
+        git url:'https://github.com/malekbenabdellatif/projet_Devops.git', branch : 'wassim-prod'
+             }
+    }
+    stage('MVN clean'){
+         steps{
+           echo 'Cleaning Project '
+           echo "Maven Version "
+           sh "mvn -Dmaven.test.failure.ignore=true clean package"
+              }
+      }
+    stage('MVN COMPILE'){
+            steps{
+                script{
+                        sh 'mvn clean install -DskipTests'
+                }
+            }
+        }
+    stage("Runing Tests with Mockito") {
+               steps{
+                   sh 'mvn test'
+                }
+        }
+    stage("MVN SonarQube") {
+        steps{
+                sh 'mvn sonar:sonar -Dsonar.login=admin -Dsonar.password=admin123 -DskipTests -X'
+            }
+        }
+    stage('Mvn Nexus'){
+        steps {
+  script {
+                    pom = readMavenPom file: "pom.xml";
+                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
+                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
+                    artifactPath = filesByGlob[0].path;
+                    artifactExists = fileExists artifactPath;
+                    if(artifactExists) {
+                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
+                        nexusArtifactUploader(
+                            nexusVersion: NEXUS_VERSION,
+                            protocol: NEXUS_PROTOCOL,
+                            nexusUrl: NEXUS_URL,
+                            groupId: pom.groupId,
+                            version: pom.version,
+                            repository: NEXUS_REPOSITORY,
+                            credentialsId: NEXUS_CREDENTIAL_ID,
+                            artifacts: [
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: artifactPath,
+                                type: pom.packaging],
+                                [artifactId: pom.artifactId,
+                                classifier: '',
+                                file: "pom.xml",
+                                type: "pom"]
+                            ]
+                        );
+                    } else {
+                        error "*** File: ${artifactPath}, could not be found";
+                    }
+                }
+        }
+   
+}
+
+    // stage('docker-compose') {
+    //   steps {
+    //       sh "docker-compose build"
+    //       sh "docker-compose up -d"
+    //       }
+    //   }
+       
+   }
+//   post {
+//         success {
+//              mail to: "wassimslim18@gmail.com",
+//                     subject: "Build sucess",
+//                     body: "sucess"
+//             echo 'successful'
+//         }
+//         failure {
+//              mail to: "wassimslim18@gmail.com",
+//                     subject: "Build failed",
+//                     body: "failed"
+//             echo 'failed'
+//         }
+//       }
+}
