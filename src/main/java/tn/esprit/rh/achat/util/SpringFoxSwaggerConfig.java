e8fc64e3ab912366f28a7d6f2399c47aa5f7423a
onschamekh <ons.chamekh@esprit.tn>
2022-11-10 18:32:24 +0100

    achat

--- src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
+++ src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
@@ -31,7 +31,7 @@ public class SpringFoxSwaggerConfig {
 				.securityContexts(Collections.singletonList(securityContext()))
 				.securitySchemes(Arrays.asList(apiKey()))
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.esprit.examen.controller"))
+				.apis(RequestHandlerSelectors.basePackage("tn.esprit.th.achat.controllers"))
 				.paths(PathSelectors.any())
 				.build();
 	}
