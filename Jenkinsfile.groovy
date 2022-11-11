7052552d82040ab0d7b29e33ba5d88ce0fe32c3e
WassimSlim21 <Wassimslim18@gmail.com>
2022-11-11 19:43:57 +0100

    adding Email to pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -132,6 +132,11 @@ pipeline {
     }
 
         }
+        post {
+        always {
+            emailext body: 'A Test EMail', recipientProviders: [[$class: 'DevelopersRecipientProvider'], [$class: 'RequesterRecipientProvider']], subject: 'Test'
+        }
+    }
 
    }
 
