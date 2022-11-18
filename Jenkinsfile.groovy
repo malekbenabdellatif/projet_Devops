557b1be2e642fae6abb24f6545fd6cbc967325e6
WassimSlim21 <Wassimslim18@gmail.com>
2022-11-18 06:36:12 +0100

    adding dockercompose up

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,122 +14,17 @@ pipeline {
         dockerImage = ''
         registry = "wassimslim/achat"
         DOCKER_CREDS_USR = "wassimslim"
+        DOCKER_CREDS = credentials('dockerhub-user-credentials')
+
             }
     
    stages{
 
-    stage('GIT') { 
-        steps{
-        git url:'https://github.com/malekbenabdellatif/projet_Devops.git', branch : 'wassim-prod'
-             }
-    }
-    stage('MAVEN clean'){
-         steps{
-           echo 'Cleaning Project '
-           echo "Maven Version "
-           sh "mvn -Dmaven.test.failure.ignore=true clean package"
-              }
-      }
-    stage('MAVEN COMPILE'){
-            steps{
-                script{
-                        sh 'mvn clean install -DskipTests'
-                }
-            }
-        }
-    stage("Runing Tests with Mockito & JUNIT TEST") {
-               steps{
-                   sh 'mvn test'
-                }
-        }
-
-
-    stage("MAVEN SonarQube") {
-        steps{
-                sh 'mvn sonar:sonar -Dsonar.login=admin -Dsonar.password=admin123 -DskipTests -X'
-            }
-        }
-    stage('MAVEN Nexus'){
-        steps {
-  script {
-                    pom = readMavenPom file: "pom.xml";
-                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
-                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
-                    artifactPath = filesByGlob[0].path;
-                    artifactExists = fileExists artifactPath;
-                    if(artifactExists) {
-                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
-                        nexusArtifactUploader(
-                            nexusVersion: NEXUS_VERSION,
-                            protocol: NEXUS_PROTOCOL,
-                            nexusUrl: NEXUS_URL,
-                            groupId: pom.groupId,
-                            version: pom.version,
-                            repository: NEXUS_REPOSITORY,
-                            credentialsId: NEXUS_CREDENTIAL_ID,
-                            artifacts: [
-                                [artifactId: pom.artifactId,
-                                classifier: '',
-                                file: artifactPath,
-                                type: pom.packaging],
-                                [artifactId: pom.artifactId,
-                                classifier: '',
-                                file: "pom.xml",
-                                type: "pom"]
-                            ]
-                        );
-                    } else {
-                        error "*** File: ${artifactPath}, could not be found";
-                    }
-                }
-        }
-        }
-
- 
-           stage('Building our image') {
-
-                steps {
-
-                    script {
-
-                    dockerImage = docker.build registry + ":$BUILD_NUMBER"
-                }
-
-            }
-
-        }
-
-            stage('Deploy our image') {
-
-                steps {
-
-                    script {
-
-                        docker.withRegistry( '', DOCKER_HUB ) {
-
-                        dockerImage.push()
-
-                    }
-
-                }
-
-            }
-
-        }
-
-            stage('Cleaning up') {
-
-                steps {
-
-                    sh "docker rmi $registry:$BUILD_NUMBER"
-
-            }
-            }
 
   
            stage('Docker Compose UP SPRING BOOT & MYSQL & Angular') { 
         steps{
-                sh "docker-compose up -d"
+                sh "docker-compose -f /springApp-mysql/docker-compose.yml up -d"
              }
     }
    }
